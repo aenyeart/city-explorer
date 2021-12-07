3bcc03e95853308f26b0d0a7055492ed36a57973
Andrew Enyeart <reallyhardtoforget@gmail.com>
2021-12-06 19:27:46 -0800

    Gets form entry sent back to App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
-import axios from 'axios'
+// import axios from 'axios'
 import CityEntry from './CityEntry.js'
-
 
 export default class App extends Component {
   constructor(props) {
@@ -13,10 +12,16 @@ export default class App extends Component {
     }
   }
 
+  formSubmission = (e) => {
+
+    console.log(e.target.cityName.value);
+    // this.setState({ search: e.target.cityName.value})
+  }
+
   render() {
     return (
       <>
-        <CityEntry />
+        <CityEntry formSubmission={this.formSubmission} />
       </>
     )
   }
--- src/CityEntry.js
+++ src/CityEntry.js
@@ -2,18 +2,22 @@ import React, { Component } from 'react'
 import Form from 'react-bootstrap/Form'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
-import InputGroup from 'react-bootstrap/InputGroup'
-import FormControl from 'react-bootstrap/FormControl'
 import Button from 'react-bootstrap/Button'
 
 
 
 
 export default class CityEntry extends Component {
+
+  formSubmission = (e) => {
+    e.preventDefault();
+    this.props.formSubmission(e);
+  }
+
   render() {
     return (
       <>
-        <Form>
+        <Form onSubmit={this.formSubmission} >
           <Row className="align-items-center">
             <Col xs="auto">
               <Form.Label htmlFor="inlineFormInput" visuallyHidden>
@@ -23,6 +27,7 @@ export default class CityEntry extends Component {
                 className="mb-2"
                 id="inlineFormInput"
                 placeholder="Enter a city name"
+                name="cityName"
               />
             </Col>
 
