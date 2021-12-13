dbc9c0f656d9bf45a125194bbed09ec8aae83ba2
Andrew Enyeart <reallyhardtoforget@gmail.com>
2021-12-12 22:19:05 -0800

    Implements cards for movie components

--- src/Movie.js
+++ src/Movie.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react'
+import Card from 'react-bootstrap/Card'
+import ListGroup from 'react-bootstrap/ListGroup'
+import ListGroupItem from 'react-bootstrap/ListGroupItem'
+
 
 export default class Movie extends Component {
   render() {
     return (
-      <>
-        <h4>{this.props.movie.title}</h4>
-        <p>{this.props.movie.overview}</p>
+      <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
         {/* <p>{this.props.movie.image_url}</p> */}
-        <p>Released: {this.props.movie.release_date}</p>
-        <p>Popularity rating: {this.props.movie.popularity}</p>
-        <p>Average audience rating / 10: {this.props.movie.vote_average}</p>
-        <p>Total audience ratings: {this.props.movie.vote_count}</p>
-      </>
+        <Card.Body>
+          <Card.Title>{this.props.movie.title}</Card.Title>
+          <Card.Text>
+            {this.props.movie.overview}
+          </Card.Text>
+        </Card.Body>
+        <ListGroup className="list-group-flush">
+          <ListGroupItem>Released: {this.props.movie.release_date}</ListGroupItem>
+          <ListGroupItem>Popularity rating: {this.props.movie.popularity}</ListGroupItem>
+          <ListGroupItem>Average audience rating: {this.props.movie.vote_average} / 10</ListGroupItem>
+          <ListGroupItem>Total audience ratings: {this.props.movie.vote_count}</ListGroupItem>
+        </ListGroup>
+      </Card>
     )
   }
 }
